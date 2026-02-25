675bdc30c2a6c7d72a546e920b0f44d3a6e5be47
Jose <jossendas@alu.edu.gva.es>
2026-02-25 22:58:58 +0100

    arreglao

--- android_app/app/src/main/java/com/example/aplicacionmovil/ui/main/SearchScreen.kt
+++ android_app/app/src/main/java/com/example/aplicacionmovil/ui/main/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
